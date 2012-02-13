0e2d49eb70d0659139325963accf8fae5fef7dfa
Igor Gerbylev <Igor.Gerbylev@bercut.com>
2012-02-13 15:51:47 +0400

    fix message severity

---
+++ few-core/src/few/Severity.java
@@ -0,0 +1,16 @@
+package few;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: gerbylev
+ * Date: 13.02.12
+ * Time: 14:49
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Severity {
+
+    INFO,
+    WARNING,
+    ERROR
+
+}
